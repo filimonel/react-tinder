9baeb7f80fa738aa4b2eb4fb5a7fe23501aefefe
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-28 19:11:00 +1000

    added material-ui packags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
-
+import Header from "./Header"
 function App() {
   return (
     <div className="App">
       <h1>Let's build this Tinder bitch yo!!</h1>
+
+      {/* Header */}
+      <Header />
+      {/* Tinder Cards */}
+      {/* Buttons below tinder cards */}
+
+      {/* Chat screen */}
+      {/* individual Chat Screen */}
+
     </div>
   );
 }
