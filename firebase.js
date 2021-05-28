6620976c0b357233be4fbda26c2c53b88cc350d0
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-28 22:27:17 +1000

    created firebase.js - added firebase config SDK to the file

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPvDO10vog_RLG4HML6R6gw9qvInJj9BQ",
+  authDomain: "tinder-clone-999a6.firebaseapp.com",
+  projectId: "tinder-clone-999a6",
+  storageBucket: "tinder-clone-999a6.appspot.com",
+  messagingSenderId: "345406820201",
+  appId: "1:345406820201:web:9dbb800f5b26d94b25b7f8",
+  measurementId: "G-H1DTGTSBE5",
+};
