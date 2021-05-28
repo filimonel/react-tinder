f3ebfd44cbdcc7c84e6d426b09ae18e2fac4d56b
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-28 19:21:13 +1000

    added Header.css

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,14 @@
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
+import "./Header.css";
+
 const Header = () => {
   return (
-    <div>
-      <h1>Hi</h1>
+    // BEM
+    <div className="header">
+      <PersonIcon />
+      <h3>Tinder</h3>
+      <ForumIcon />
     </div>
   );
 };
