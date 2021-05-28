8a1c610d99692c6dfe14654beb263a2d2be73aca
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-28 21:58:37 +1000

    added TinderCards.css - updated styling for tinder cards

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,9 +1,40 @@
 import { useState } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
 
 const TinderCards = () => {
   // This is how you declare a variable in the react way
-  const [people, setPeople] = useState([]);
-  return <div></div>;
+  // setPeople([...people, 'sonny', 'qazi']) - react way. push to array
+  const [people, setPeople] = useState([
+    {
+      name: "Steve Jobs",
+      url: "https://media.gettyimages.com/photos/apple-ceo-steve-jobs-speaks-during-an-apple-special-event-april-8-in-picture-id98328574?s=2048x2048",
+    },
+    {
+      name: "Steve Wozniak",
+      url: "https://cdn.hipwallpaper.com/i/1/65/c6g1aH.jpg",
+    },
+  ]);
+
+  return (
+    <div>
+      <h1>Tinder Cards</h1>
+      {people.map((person) => (
+        <TinderCard
+          className="swipe"
+          key={person.name}
+          preventSwipe={["up", "down"]}
+        >
+          <div
+            className="card"
+            style={{ backgroundImage: `url(${person.url})` }}
+          >
+            <h3>{person.name}</h3>
+          </div>
+        </TinderCard>
+      ))}
+    </div>
+  );
 };
 
 export default TinderCards;
