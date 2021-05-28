7d2c7dbaa8d4770cdb4d19f29252b9a0c72cbc33
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-28 22:17:52 +1000

    styled tinder cards to stack upon each other - cards are now centered

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -7,32 +7,38 @@ const TinderCards = () => {
   // setPeople([...people, 'sonny', 'qazi']) - react way. push to array
   const [people, setPeople] = useState([
     {
-      name: "Steve Jobs",
-      url: "https://media.gettyimages.com/photos/apple-ceo-steve-jobs-speaks-during-an-apple-special-event-april-8-in-picture-id98328574?s=2048x2048",
+      name: "Satoru Gojo",
+      url: "https://c4.wallpaperflare.com/wallpaper/787/854/424/jujutsu-kaisen-satoru-gojo-anime-boys-anime-girls-hd-wallpaper-preview.jpg",
     },
     {
-      name: "Steve Wozniak",
-      url: "https://cdn.hipwallpaper.com/i/1/65/c6g1aH.jpg",
+      name: "Kakashi Hatake",
+      url: "https://c4.wallpaperflare.com/wallpaper/404/527/706/naruto-kakashi-hatake-hd-wallpaper-preview.jpg",
+    },
+    {
+      name: "Erza Scarlet",
+      url: "https://images.alphacoders.com/744/thumb-1920-744311.png",
     },
   ]);
 
   return (
     <div>
       <h1>Tinder Cards</h1>
-      {people.map((person) => (
-        <TinderCard
-          className="swipe"
-          key={person.name}
-          preventSwipe={["up", "down"]}
-        >
-          <div
-            className="card"
-            style={{ backgroundImage: `url(${person.url})` }}
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
           >
-            <h3>{person.name}</h3>
-          </div>
-        </TinderCard>
-      ))}
+            <div
+              className="card"
+              style={{ backgroundImage: `url(${person.url})` }}
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
     </div>
   );
 };
