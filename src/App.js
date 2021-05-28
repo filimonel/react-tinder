9d8c532e3e07b005735cc19483a4c8f7489fef11
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-28 20:03:09 +1000

    established routes for homepage and chat

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+
 function App() {
   return (
     <div className="App">
-      {/* Header */}
       <Header />
-      {/* Tinder Cards */}
-      {/* Buttons below tinder cards */}
+      <Router>
+        <Switch>
+          {/* Header */}
+          <Route path="/" exact>
+            <h1>Homepage</h1>
+          </Route>
+          <Route path="/chat" exact>
+            {/* Chat screen */}
+            <h1>Chat</h1>
+          </Route>
+          {/* Tinder Cards */}
+          {/* Buttons below tinder cards */}
 
-      {/* Chat screen */}
-      {/* individual Chat Screen */}
+          {/* individual Chat Screen */}
+        </Switch>
+      </Router>
     </div>
   );
 }
