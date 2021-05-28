e07f0a648cb38106afc0bc0dbd91fe76a58c03e7
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-28 22:31:00 +1000

    added firebase package to package.json - updated firebase.js file to enable access to database

--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAPvDO10vog_RLG4HML6R6gw9qvInJj9BQ",
   authDomain: "tinder-clone-999a6.firebaseapp.com",
@@ -8,3 +10,8 @@ const firebaseConfig = {
   appId: "1:345406820201:web:9dbb800f5b26d94b25b7f8",
   measurementId: "G-H1DTGTSBE5",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const database = firebaseApp.firestore();
+
+export default database;
