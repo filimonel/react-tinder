b99844ae565320b5b9ab39201b9a4f92cf463e63
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-29 11:40:38 +1000

    finished styling for buttons

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -4,15 +4,30 @@ import CloseIcon from "@material-ui/icons/Close";
 import StarRateIcon from "@material-ui/icons/StarRate";
 import FavouriteIcon from "@material-ui/icons/Favorite";
 import FlashOnIcon from "@material-ui/icons/FlashOn";
+import IconButton from "@material-ui/core/IconButton";
 
 const SwipeButtons = () => {
   return (
     <div className="swipeButtons">
-      <ReplayIcon fontSize="large" />
-      <CloseIcon fontSize="large" />
-      <StarRateIcon fontSize="large" />
-      <FavouriteIcon fontSize="large" />
-      <FlashOnIcon fontSize="large" />
+      <IconButton className="swipeButtons__repeat">
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+
+      <IconButton className="swipeButtons__left">
+        <CloseIcon fontSize="large" />
+      </IconButton>
+
+      <IconButton className="swipeButtons__star">
+        <StarRateIcon fontSize="large" />
+      </IconButton>
+
+      <IconButton className="swipeButtons__right">
+        <FavouriteIcon fontSize="large" />
+      </IconButton>
+
+      <IconButton className="swipeButtons__lighting">
+        <FlashOnIcon fontSize="large" />
+      </IconButton>
     </div>
   );
 };
